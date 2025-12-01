a92c3fa301d34fa5ee783b5c64d38a1610288f8c
sjberenato <sjberenato@gmail.com>
2025-12-01 15:46:11 -0700

    Add files via upload

---
+++ enPlaceApp.swift
@@ -0,0 +1,32 @@
+//
+//  enPlaceApp.swift
+//  enPlace
+//
+//  Created by Sam Berenato on 12/1/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct enPlaceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ enPlaceTests.swift
@@ -0,0 +1,16 @@
+//
+//  enPlaceTests.swift
+//  enPlaceTests
+//
+//  Created by Sam Berenato on 12/1/25.
+//
+
+import Testing
+
+struct enPlaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
